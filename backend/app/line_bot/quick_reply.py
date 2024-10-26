e4c8d493ca85a931844300fb5d82dcda61fbde2a
Omusubi0123 <Vermouth77rseidl@gmail.com>
2024-10-27 05:09:00 +0900

    feat: RAG検索作成

--- backend/app/line_bot/quick_reply.py
+++ backend/app/line_bot/quick_reply.py
@@ -1,13 +1,9 @@
-from datetime import datetime
-
 from linebot.models import QuickReply, TextSendMessage
 
 from app.alg.summarize_diary import summarize_diary_by_llm
 from app.db.add_diary_summary import add_diary_summary
-from app.db.manage_user_status import update_user_status
 from app.line_bot.flex_message import create_flex_message
 from app.line_bot.quick_reply_item import create_quick_reply_buttons, create_reply_text
-from app.line_bot_settings import line_bot_api
 from app.utils.data_enum import QuickReplyField
 
 
@@ -49,4 +45,4 @@ def create_quick_reply(
     if flex_message:
         messages.insert(0, flex_message)
 
-    line_bot_api.reply_message(event.reply_token, messages)
+    return messages
