d8c4604ac84b6784f8deb37b033f95c906a93f69
Omusubi0123 <Vermouth77rseidl@gmail.com>
2024-10-27 02:28:00 +0900

    refac: user schemaを別ファイルに移動

--- backend/app/schemas/diary_schema.py
+++ backend/app/schemas/diary_schema.py
@@ -31,19 +31,3 @@ class FetchDiary(BaseModel):
     day: int
 
 
-class User(BaseModel):
-    user_id: str
-    display_name: str
-    picture_url: str
-    status_message: str
-    created_at: datetime
-    updated_at: datetime
-    linkToken: str
-
-    personality: str
-    strength: str
-    weakness: str
-
-
-class FetchProfile(BaseModel):
-    user_id: str
