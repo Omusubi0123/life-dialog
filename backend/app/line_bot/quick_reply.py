4d97b26c314bc4f1eb02f7ea5c086b85e89dbabe
andotnct <mityugame@gmail.com>
2024-10-26 23:20:46 +0900

    refac: statusとquick_reply_buttonsの生成を関数に切り分け

--- backend/app/line_bot/quick_reply.py
+++ backend/app/line_bot/quick_reply.py
@@ -12,6 +12,32 @@ from app.line_bot_settings import line_bot_api
 from app.settings import settings
 from app.utils.data_enum import QuickReplyField
 
+def get_current_status(event):
+    if event.message.text == QuickReplyField.diary_mode.value:
+        return QuickReplyField.diary_mode.value
+    elif event.message.text == QuickReplyField.interactive_mode.value:
+        return QuickReplyField.interactive_mode.value
+    else:
+        return get_user_status(event.source.user_id)
+
+def create_quick_reply_buttons(status):
+    quick_reply_items = []
+    if status == QuickReplyField.diary_mode.value:
+        quick_reply_items.append(QuickReplyField.interactive_mode.value)
+    elif status == QuickReplyField.interactive_mode.value:
+        quick_reply_items.append(QuickReplyField.diary_mode.value)
+    quick_reply_items.append(QuickReplyField.view_diary.value)
+    
+    quick_reply_buttons = [
+        QuickReplyButton(
+            action=MessageAction(label=item, text=item),
+            image_url="https://firebasestorage.googleapis.com/v0/b/jp-hacks-77212.appspot.com/o/image%2Fdiary.png?alt=media&token=267f1791-dbd1-4095-9c1a-e39eb7ade290"
+        )
+        for item in quick_reply_items
+    ]
+
+    return quick_reply_buttons
+
 def create_flex_message(user_id: str):
     flex_message = FlexSendMessage(
         alt_text='複数のカードメッセージ',
@@ -37,12 +63,12 @@ def create_flex_message(user_id: str):
                                 "weight": "bold",
                                 "size": "xl"
                             },
-                            # {
-                            #     "type": "text",
-                            #     "text": "カード 1 要約要約要約要約要約",
-                            #     "size": "md",
-                            #     "wrap": True
-                            # }
+                            {
+                                "type": "text",
+                                "text": "この日は図書館で勉強を頑張り、家に帰ってからはドラマを見た。夜ご飯は好物をお母さんが作ってくれて...",
+                                "size": "md",
+                                "wrap": True
+                            }
                         ]
                     },
                     "footer": {
@@ -67,33 +93,15 @@ def create_flex_message(user_id: str):
 
 def create_quick_reply(event, reply_text: str):
     user_id = event.source.user_id
+    status = get_current_status(event)
+    update_user_status(user_id, status)
+    quick_reply_buttons = create_quick_reply_buttons(status)
+
     flex_message = None
-    
-    quick_reply_items = []
     if event.message.text == QuickReplyField.view_diary.value:
         flex_message = create_flex_message(user_id)
-    elif event.message.text == QuickReplyField.diary_mode.value:
-        update_user_status(user_id, QuickReplyField.diary_mode.value)
-    elif event.message.text == QuickReplyField.interactive_mode.value:
-        update_user_status(user_id, QuickReplyField.interactive_mode.value)
-    else:
-        status = get_user_status(user_id)
-        if status == QuickReplyField.interactive_mode.value:
-            pass
+        reply_text = "この日は寝坊をしちゃったんだね。でも午後は数学の勉強を頑張れたみたいで良いじゃん！"
 
-
-    quick_reply_items.append(QuickReplyField.diary_mode.value)
-    quick_reply_items.append(QuickReplyField.interactive_mode.value)
-    quick_reply_items.append(QuickReplyField.view_diary.value)
-    
-    quick_reply_buttons = [
-        QuickReplyButton(
-            action=MessageAction(label=item, text=item),
-            image_url="https://firebasestorage.googleapis.com/v0/b/jp-hacks-77212.appspot.com/o/image%2Fdiary.png?alt=media&token=267f1791-dbd1-4095-9c1a-e39eb7ade290"
-        )
-        for item in quick_reply_items
-    ]
-    
     quick_reply_message = TextSendMessage(
         text=reply_text,
         quick_reply=QuickReply(items=quick_reply_buttons)
@@ -108,23 +116,8 @@ def create_quick_reply(event, reply_text: str):
         messages
     )
 
-
-    # 全てのモードで「日付選択」は表示する
-    # extra_items = [QuickReplyField.day_choice.value]
-    # datetime_picker_action = DatetimePickerTemplateAction(
-    #     label=QuickReplyField.day_choice.value,
-    #     data="action=select_date",
-    #     mode="date",
-    #     initial="2024-04-24",
-    #     min="2024-01-01",
-    #     max="2024-12-31",
-    # )
-    # quick_reply_buttons += [
-    #     QuickReplyButton(action=datetime_picker_action, image_url=image_url)
-    #     for item in extra_items
-    # ]
-
     quick_reply = QuickReply(items=quick_reply_buttons)
     line_bot_api.reply_message(
         event.reply_token, TextSendMessage(text=reply_text, quick_reply=quick_reply)
     )
+
