e61b7c5354971736e0a94f3169aa4747c53877f3
Omusubi0123 <Vermouth77rseidl@gmail.com>
2024-10-27 03:54:02 +0900

    refac: quick replyのtextと画像を別ファイルに移動

--- backend/app/line_bot/quick_reply.py
+++ backend/app/line_bot/quick_reply.py
@@ -1,19 +1,13 @@
-from datetime import datetime
 import random
+from datetime import datetime
 
-from linebot.models import (
-    DatetimePickerTemplateAction,
-    FlexSendMessage,
-    MessageAction,
-    QuickReply,
-    QuickReplyButton,
-    TextSendMessage,
-)
+from linebot.models import FlexSendMessage, QuickReply, TextSendMessage
 
-from app.db.get_diary import get_diary_from_db
 from app.alg.summarize_diary import summarize_diary_by_llm
 from app.db.add_diary_summary import add_diary_summary
+from app.db.get_diary import get_diary_from_db
 from app.db.manage_user_status import get_user_status, update_user_status
+from app.line_bot.quick_reply_item import create_quick_reply_buttons, create_reply_text
 from app.line_bot_settings import line_bot_api
 from app.settings import settings
 from app.utils.data_enum import QuickReplyField
@@ -26,65 +20,33 @@ def get_current_status(event):
         return QuickReplyField.interactive_mode.value
     else:
         return get_user_status(event.source.user_id)
-    
+
+
 def create_summary_feedback(event, year, month, day):
-  user_id = event.source.user_id
+    user_id = event.source.user_id
 
-  if event.message.text == QuickReplyField.view_diary.value:
-    summary, feedback = summarize_diary_by_llm(user_id, year, month, day)
-    add_diary_summary(user_id, summary, feedback, year, month, day)
-    return summary, feedback
-  else:
-      return None, None
-
-
-def create_quick_reply_buttons(status):
-    quick_reply_items = []
-    images = []
-    if status == QuickReplyField.diary_mode.value:
-        quick_reply_items.append(QuickReplyField.interactive_mode.value)
-        images.append(
-            f"https://firebasestorage.googleapis.com/v0/b/{settings.gcs_bucket_name}/o/material%2Fdialogue_green.png?alt=media&token=51013bcc-86c8-4bca-9da4-d627e1b6424f"
-        )
-        images.append(
-            f"https://firebasestorage.googleapis.com/v0/b/{settings.gcs_bucket_name}/o/material%2Fbook_green.png?alt=media&token=0d17b006-6bf7-4070-8454-ed7c967ef4d9"
-        )
-    elif status == QuickReplyField.interactive_mode.value:
-        quick_reply_items.append(QuickReplyField.diary_mode.value)
-        images.append(
-            f"https://firebasestorage.googleapis.com/v0/b/{settings.gcs_bucket_name}/o/material%2Fpen_blue.png?alt=media&token=0ef43729-b0c8-4c4b-9d9c-7c9371d5b1c6"
-        )
-        images.append(
-            f"https://firebasestorage.googleapis.com/v0/b/{settings.gcs_bucket_name}/o/material%2Fbook_blue.png?alt=media&token=bc5dd23f-3db0-4e81-aff3-9c60aab75fb3"
-        )
-    quick_reply_items.append(QuickReplyField.view_diary.value)
-
-    quick_reply_buttons = [
-        QuickReplyButton(action=MessageAction(label=item, text=item), image_url=image)
-        for item, image in zip(quick_reply_items, images)
-    ]
-
-    return quick_reply_buttons
-
-
-def create_reply_text(event, feedback):
-    if event.message.text == QuickReplyField.diary_mode.value:
-        return "【人生を記録】\n日々の生活を記録しよう！\n画像も送信できるよ♪"
-    elif event.message.text == QuickReplyField.interactive_mode.value:
-        return "【人生と対話】\n何について話す？\n日記を探すこともできるよ♪"
-    elif event.message.text == QuickReplyField.view_diary.value:
-        return feedback
+    if event.message.text == QuickReplyField.view_diary.value:
+        summary, feedback = summarize_diary_by_llm(user_id, year, month, day)
+        add_diary_summary(user_id, summary, feedback, year, month, day)
+        return summary, feedback
     else:
-        return "送信ありがとう♪"
-    
+        return None, None
+
+
 def get_diary_random_image(user_id, year, month, day):
-    doc_dict = get_diary_from_db(
-        user_id, year, month, day
-    )
-    if 'files' in doc_dict and isinstance(doc_dict['files'], dict) and len(doc_dict['files']) > 0:
+    doc_dict = get_diary_from_db(user_id, year, month, day)
+    if (
+        "files" in doc_dict
+        and isinstance(doc_dict["files"], dict)
+        and len(doc_dict["files"]) > 0
+    ):
         # 'mediatype'が'image'のものだけを抽出
-        image_files = [file_data['url'] for file_data in doc_dict['files'].values() if file_data['mediatype'] == 'image']
-        
+        image_files = [
+            file_data["url"]
+            for file_data in doc_dict["files"].values()
+            if file_data["mediatype"] == "image"
+        ]
+
         if image_files:
             return random.choice(image_files)
         else:
@@ -107,7 +69,9 @@ def create_flex_message(event, status, summary, year, month, day):
                         "type": "bubble",
                         "hero": {
                             "type": "image",
-                            "url": thumbnail_image_url if thumbnail_image_url else "https://firebasestorage.googleapis.com/v0/b/jp-hacks-77212.appspot.com/o/material%2Fdefault_diary_thumbnail.jpg?alt=media&token=9aad0b1e-04e4-4727-97a6-2668de248d02",
+                            "url": thumbnail_image_url
+                            if thumbnail_image_url
+                            else "https://firebasestorage.googleapis.com/v0/b/jp-hacks-77212.appspot.com/o/material%2Fdefault_diary_thumbnail.jpg?alt=media&token=9aad0b1e-04e4-4727-97a6-2668de248d02",
                             "size": "full",
                             "aspectRatio": "20:13",
                             "aspectMode": "cover",
@@ -157,12 +121,18 @@ def create_flex_message(event, status, summary, year, month, day):
 
 
 def create_quick_reply(event):
+    """"""
     user_id = event.source.user_id
     today = datetime.now()
     status = get_current_status(event)
-    update_user_status(user_id, status)
+    text = event.message.text
 
-    summary, feedback = create_summary_feedback(event, today.year, today.month, today.day)
+    if text in QuickReplyField.get_values() and text != status:
+        update_user_status(user_id, status)
+
+    summary, feedback = create_summary_feedback(
+        event, today.year, today.month, today.day
+    )
 
     reply_text = create_reply_text(event, feedback)
     quick_reply_buttons = create_quick_reply_buttons(status)
@@ -172,7 +142,9 @@ def create_quick_reply(event):
     )
 
     messages = [quick_reply_message]
-    flex_message = create_flex_message(event, status, summary, today.year, today.month, today.day)
+    flex_message = create_flex_message(
+        event, status, summary, today.year, today.month, today.day
+    )
     if flex_message:
         messages.insert(0, flex_message)
 
---
+++ backend/app/line_bot/quick_reply_item.py
@@ -0,0 +1,44 @@
+from linebot.models import MessageAction, QuickReplyButton
+
+from app.settings import settings
+from app.utils.data_enum import QuickReplyField
+
+
+def create_reply_text(event, feedback):
+    if event.message.text == QuickReplyField.diary_mode.value:
+        return "【人生を記録】\n日々の生活を記録しよう！\n画像も送信できるよ♪"
+    elif event.message.text == QuickReplyField.interactive_mode.value:
+        return "【人生と対話】\n何について話す？\n日記を探すこともできるよ♪"
+    elif event.message.text == QuickReplyField.view_diary.value:
+        return feedback
+    else:
+        return "送信ありがとう♪"
+
+
+def create_quick_reply_buttons(status):
+    quick_reply_items = []
+    images = []
+    if status == QuickReplyField.diary_mode.value:
+        quick_reply_items.append(QuickReplyField.interactive_mode.value)
+        images.append(
+            f"https://firebasestorage.googleapis.com/v0/b/{settings.gcs_bucket_name}/o/material%2Fdialogue_green.png?alt=media&token=51013bcc-86c8-4bca-9da4-d627e1b6424f"
+        )
+        images.append(
+            f"https://firebasestorage.googleapis.com/v0/b/{settings.gcs_bucket_name}/o/material%2Fbook_green.png?alt=media&token=0d17b006-6bf7-4070-8454-ed7c967ef4d9"
+        )
+    elif status == QuickReplyField.interactive_mode.value:
+        quick_reply_items.append(QuickReplyField.diary_mode.value)
+        images.append(
+            f"https://firebasestorage.googleapis.com/v0/b/{settings.gcs_bucket_name}/o/material%2Fpen_blue.png?alt=media&token=0ef43729-b0c8-4c4b-9d9c-7c9371d5b1c6"
+        )
+        images.append(
+            f"https://firebasestorage.googleapis.com/v0/b/{settings.gcs_bucket_name}/o/material%2Fbook_blue.png?alt=media&token=bc5dd23f-3db0-4e81-aff3-9c60aab75fb3"
+        )
+    quick_reply_items.append(QuickReplyField.view_diary.value)
+
+    quick_reply_buttons = [
+        QuickReplyButton(action=MessageAction(label=item, text=item), image_url=image)
+        for item, image in zip(quick_reply_items, images)
+    ]
+
+    return quick_reply_buttons
