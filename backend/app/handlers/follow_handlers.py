2ccae51186827011ed69d89645f45c2cbcb2e980
andotnct <mityugame@gmail.com>
2024-10-27 09:56:10 +0900

    feat: line userのnameやiconを取得しdbに保存

--- backend/app/handlers/follow_handlers.py
+++ backend/app/handlers/follow_handlers.py
@@ -10,11 +10,20 @@ settings = Settings()
 channel_access_token = settings.channel_access_token
 
 
-def handle_follow_event(event):
-    """公式アカウントに登録された時、ユーザードキュメントと日記コレクションを作成"""
-    user_id = event.source.user_id
-    timestamp = timestamp_md_to_datetime(event.timestamp)
+def get_user_profile(user_id):
+    url = f'https://api.line.me/v2/bot/profile/{user_id}'
+    headers = {
+        'Authorization': f'Bearer {settings.channel_access_token}'
+    }
+    response = requests.get(url, headers=headers)
 
+    if response.status_code == 200:
+        print("Success:", response.json())
+        return response.json()
+    else:
+        return None
+
+def get_user_link_token(user_id):
     url = f"https://api.line.me/v2/bot/user/{user_id}/linkToken"
     headers = {
         "Authorization": f"Bearer {channel_access_token}",
@@ -24,8 +33,24 @@ def handle_follow_event(event):
     if response.status_code == 200:
         print("Success:", response.json())
         link_token = response.json()["linkToken"]
+        return link_token
+    else:
+        return None
+
+def handle_follow_event(event):
+    """公式アカウントに登録された時、ユーザードキュメントと日記コレクションを作成"""
+    user_id = event.source.user_id
+    timestamp = timestamp_md_to_datetime(event.timestamp)
+    
+    link_token = get_user_link_token(user_id)
+    user_profile = get_user_profile(user_id)
+
+    if link_token and user_profile:
         user_doc_field = {
             UserField.user_id.value: user_id,
+            UserField.user_name.value: user_profile["displayName"],
+            UserField.icon_url.value: user_profile["pictureUrl"] if "pictureUrl" in user_profile else "",
+            UserField.status_message.value: user_profile["statusMessage"] if "statusMessage" in user_profile else "",
             UserField.created_at.value: timestamp,
             UserField.linkToken.value: link_token,
         }
