4841bb6f69d507f5f944fb4a455878c67d161a96
andotnct <mityugame@gmail.com>
2024-10-27 13:43:10 +0900

    fix: frontの日付の時間バグを修正

--- frontend/src/utils/getTimeFromTimeStamp.ts
+++ frontend/src/utils/getTimeFromTimeStamp.ts
@@ -1,6 +1,11 @@
 export default function getTimeFromTimeStamp(timestamp: string): string {
     const date = new Date(timestamp);
-    const hours = String(date.getUTCHours()).padStart(2, '0');
+    let hours: number = date.getUTCHours() + 9; // UTC時間に9時間を加算
+    if (hours >= 24) {
+        hours -= 24;
+    }
+    const formattedHours: string = String(hours).padStart(2, '0');
+
     const minutes = String(date.getUTCMinutes()).padStart(2, '0');
-    return `${hours}:${minutes}`;
+    return `${formattedHours}:${minutes}`;
 }
