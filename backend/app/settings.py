e2f1edde23d6e39960b9961107d7f168f3d89b81
Omusubi0123 <Vermouth77rseidl@gmail.com>
2025-05-18 15:48:29 +0900

    feat: 不要なsettings, library削除

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -6,14 +6,7 @@ class Settings(BaseSettings):
     channel_secret: str
     channel_access_token: str
 
-    firebase_credential: str
-    firebase_project_id: str
-    gcs_bucket_name: str
-
     openai_api_key: str
-    azure_ai_search_endpoint: str
-    azure_ai_search_admin_key: str
-    azure_ai_search_index_name: str
 
     database_url: str
 
