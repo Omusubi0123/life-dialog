a0354e4a27f8ad8b1b2dd781a9008c4cb99364ea
Omusubi0123 <Omubusi0123@github.com>
2025-08-24 19:34:22 +0900

    chore

--- backend/app/alg/search_cosine_sim.py
+++ backend/app/alg/search_cosine_sim.py
@@ -1,15 +1,15 @@
 from sqlalchemy import text
 
 from app.alg.print_search_result import print_search_result
+from app.db.session import session_scope
 from app.utils.llm_response import get_embedding
-from app.utils.session_scope import get_session
 
 
 def cosine_similar_diary(
     user_id: str, query: str, top_k: int = 4, debug: bool = False
 ) -> list[dict]:
     """指定したユーザーの日記の中から、クエリに最も類似した日記を取得"""
-    with get_session() as session:
+    with session_scope() as session:
         query_vector: list[float] = get_embedding(query)
         sql_query = text(
             f"""
--- backend/app/utils/data_enum.py
+++ backend/app/utils/data_enum.py
@@ -18,46 +18,6 @@ class QuickReplyField(Enum):
         return [item.name for item in cls]
 
 
-class AnalyzeUserField(Enum):
-    """LLMが分析したユーザーの情報"""
-
-    personality = "personality"
-    strength = "strength"
-    weakness = "weakness"
-
-
-class RootCollection(Enum):
-    """DBのルートコレクション"""
-
-    user = "Users"
-    diary = "Diary"
-
-
-class UserField(Enum):
-    """DBのユーザードキュメントフィールド"""
-
-    user_id = "user_id"
-
-    user_name = "user_name"
-    icon_url = "icon_url"
-    status_message = "status_message"
-
-    status = "status"
-    created_at = "created_at"
-    updated_at = "updated_at"
-    linkToken = "linkToken"
-
-    personality = "personality"
-    strength = "strength"
-    weakness = "weakness"
-
-
-class DiaryCollection(Enum):
-    """DBの日記コレクション"""
-
-    diary = "diary"
-
-
 class DiaryField(Enum):
     """DBの日記ドキュメントフィールド"""
 
@@ -69,20 +29,3 @@ class DiaryField(Enum):
     feedback = "feedback"
     texts = "texts"
     files = "files"
-
-
-class TextField(Enum):
-    """DBの日記ドキュメントのテキストフィールド"""
-
-    key = "text{}"
-    text = "text"
-    timestamp = "timestamp"
-
-
-class FileField(Enum):
-    """DBの日記ドキュメントのファイルフィールド"""
-
-    key = "file{}"
-    url = "url"
-    mediatype = "mediatype"
-    timestamp = "timestamp"
